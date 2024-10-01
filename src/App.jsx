aa58b9bddbb351c21d5806c00b8a1a7147b2ef73
dasom222g <dasom228@gmail.com>
2024-10-01 22:31:05 +0900

    Add firebase function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { auth } from "./firebase";
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./pages/PrivateRoute";
@@ -13,39 +13,39 @@ import Edit from "./pages/Edit";
 function App() {
   // logic
   const [editItem, setEditItem] = useState(null);
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <PrivateRoute />,
-      children: [
-        {
-          path: "",
-          element: <Home onEdit={(item) => setEditItem(item)} />,
-        },
-        {
-          path: "/post",
-          element: <Post />,
-        },
-        {
-          path: "/edit",
-          element: <Edit item={editItem} />,
-        },
-        {
-          path: "/profile",
-          element: <Profile />,
-        },
-      ],
-    },
-    // 각 객체 하나가 페이지 하나
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/sign-up",
-      element: <SignUp />,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <PrivateRoute />,
+  //     children: [
+  //       {
+  //         path: "",
+  //         element: <Home onEdit={(item) => setEditItem(item)} />,
+  //       },
+  //       {
+  //         path: "/post",
+  //         element: <Post />,
+  //       },
+  //       {
+  //         path: "/edit",
+  //         element: <Edit item={editItem} />,
+  //       },
+  //       {
+  //         path: "/profile",
+  //         element: <Profile />,
+  //       },
+  //     ],
+  //   },
+  //   // 각 객체 하나가 페이지 하나
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
+  //   {
+  //     path: "/sign-up",
+  //     element: <SignUp />,
+  //   },
+  // ]);
 
   const [isLoading, setIsLoading] = useState(true); // 진입시 무조건 로딩
 
@@ -68,16 +68,29 @@ function App() {
   return (
     <div className="bg-churead-black h-full text-white overflow-auto">
       <div className="max-w-[572px] mx-auto h-full">
-        {/* <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/post" element={<Post />} />
-          </Routes>
-        </BrowserRouter> */}
-        {isLoading ? <p>Loading...</p> : <RouterProvider router={router} />}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <BrowserRouter>
+            <Routes>
+              {/* 중첩 라우팅 */}
+              <Route element={<PrivateRoute />}>
+                <Route
+                  path="/"
+                  element={<Home onEdit={(item) => setEditItem(item)} />}
+                />
+                <Route path="/post" element={<Post />} />
+                <Route path="/edit" element={<Edit item={editItem} />} />
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+
+              <Route path="/login" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </BrowserRouter>
+        )}
+
+        {/* {isLoading ? <p>Loading...</p> : <RouterProvider router={router} />} */}
       </div>
     </div>
   );
